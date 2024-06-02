50ce75de2374384b61eed3c1cc9ea452d3eb02dc
sakhb <druzhininsergey96@gmail.com>
2024-06-03 04:13:09 +0700

    SC-02 implement dagger hilt di

--- app/src/main/java/com/example/sakhcast/MainActivity.kt
+++ app/src/main/java/com/example/sakhcast/MainActivity.kt
@@ -6,13 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.sakhcast.ui.MainScreen
 import com.example.sakhcast.ui.theme.SakhCastTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                or View.SYSTEM_UI_FLAG_FULLSCREEN
-//                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
 
         enableEdgeToEdge()
         setContent {
