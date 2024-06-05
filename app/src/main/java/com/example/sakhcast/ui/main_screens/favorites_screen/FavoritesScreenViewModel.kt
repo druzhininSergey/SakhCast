067a3709003298124d092ed6827b694e35e3c092
sakhb <druzhininsergey96@gmail.com>
2024-06-06 06:46:48 +0700

    SC-07 FavoritesScreenViewModel

---
+++ app/src/main/java/com/example/sakhcast/ui/main_screens/favorites_screen/FavoritesScreenViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.sakhcast.ui.main_screens.favorites_screen
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.sakhcast.data.Samples
+import com.example.sakhcast.model.MovieCard
+import com.example.sakhcast.model.SeriesCard
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FavoritesScreenViewModel @Inject constructor(): ViewModel() {
+
+    private var _favoritesScreenState = MutableLiveData(FavoritesScreenState())
+    val favoritesScreenState: LiveData<FavoritesScreenState> = _favoritesScreenState
+
+    init {
+        getAllContent()
+    }
+
+    data class FavoritesScreenState(
+        var seriesCardWatching: List<SeriesCard> = emptyList(),
+        var seriesCardWillWatch: List<SeriesCard> = emptyList(),
+        var seriesCardFinishedWatching: List<SeriesCard> = emptyList(),
+        var movieCardsWillWatch: List<MovieCard> = emptyList(),
+    )
+
+    fun getAllContent(){
+        _favoritesScreenState.value = favoritesScreenState.value?.copy(
+            seriesCardWatching = Samples.getAllSeries(),
+            seriesCardWillWatch = Samples.getAllSeries(),
+            seriesCardFinishedWatching = Samples.getAllSeries(),
+            movieCardsWillWatch = Samples.getAllMovies(),
+        )
+    }
+
+}
